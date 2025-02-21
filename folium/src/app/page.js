3bda8b41c74d5624b72f8099c65e761c96d5699f
Zyanya <hey.zyanlml@gmail.com>
2025-02-20 19:50:08 -0600

    Add page js

--- folium/src/app/page.js
+++ folium/src/app/page.js
@@ -1,11 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import NavBar from "./components/NavBar/NavBar";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <NavBar />
-    </div>
-  );
+  return <div className={styles.page}></div>;
 }
